4904a282e9fe9b876bb173605ac7fd493ec3bb7d
Otavio Vanderlei <otaviovanderlei1743@gmail.com>
2026-04-26 14:14:04 -0300

    frontend pronto

---
+++ src/app/app.ts
@@ -0,0 +1,46 @@
+import { Component, signal } from '@angular/core';
+import { Tarefa } from './tarefa';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('TODOapp');
+
+ arrayDeTarefas = signal<Tarefa[]>([]);
+  apiURL : string;
+  constructor(private http: HttpClient) {
+    this.apiURL = 'https://projeto-angular-crud.onrender.com';
+    this.READ_tarefa();
+  }
+
+CREATE_tarefa(descricaoNovaTarefa: string) {
+    var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+     this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefa(); });
+}
+
+READ_tarefa() {
+    this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(
+      resultado => this.arrayDeTarefas.set(resultado));
+}
+
+DELETE_tarefa(tarefaAserRemovida: Tarefa) {
+    var indice = this.arrayDeTarefas().indexOf(tarefaAserRemovida); 
+    var id = this.arrayDeTarefas()[indice]._id;                     
+    this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefa(); });
+  
+  }
+ UPDATE_tarefa(tarefaAserModificada: Tarefa) {
+    var indice = this.arrayDeTarefas().indexOf(tarefaAserModificada); 
+    var id = this.arrayDeTarefas()[indice]._id;                      
+    this.http.patch<Tarefa>(`${this.apiURL}/api/update/${id}`,
+      tarefaAserModificada).subscribe(
+        resultado => { console.log(resultado); this.READ_tarefa(); });
+  }
+}
